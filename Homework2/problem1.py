741f8c4f17eff878577679eb3f1288e2d97cfd16
rosiehyan <rozahovhannisyan98@gmail.com>
2018-11-09 21:08:43 +0400

    add Homework2

---
+++ Homework2/problem1.py
@@ -0,0 +1,5 @@
+num = int(input("Enter an integer: "))
+if(num%2==0) : 
+  print("It's even number")  
+else: 
+  print("It's odd number") 
---
+++ Homework2/problem2.py
@@ -0,0 +1,21 @@
+import random
+
+num = random.randint(1, 100)
+num_of_guesses = 0
+guess = 0
+ 
+while int(guess) != num: 
+  guess = input("Guess: ") 
+  if guess == "exit":
+     break
+ 
+  num_of_guesses += 1 
+
+  if int(guess) > num:
+    print("It's too high")  
+  elif int(guess) < num:
+    print("It's too low")
+else:
+  print("Congrats")
+  print("Bravo. You guessed it in {} steps {}" .format(num_of_guesses, 42))  
+
